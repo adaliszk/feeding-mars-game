efa688593cc301492f09dcef6c319d2ff7a5f6ac
Ádám Liszkai <adaliszk@gmail.com>
2020-04-10 21:56:55 +0200

    add (LoaderScene): basic loader UI + load SCENE using GAME-DATA

---
+++ Assets/Data/Game.cs
@@ -0,0 +1,24 @@
+﻿namespace Data
+{
+    // ------------------------------------------------------------------------
+    public class GameData
+    {
+        public string CurrentScene = "Sandbox";
+
+        public void SetScene(string sceneName)
+        {
+            CurrentScene = sceneName;
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    public static class Game
+    {
+        private static GameData _currentData;
+
+        public static GameData GetData()
+        {
+            return _currentData ?? (_currentData = new GameData());
+        }
+    }
+}
---
+++ Assets/Scenes/LoaderScene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Threading;
+using Data;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Scenes
+{
+    public class LoaderScene : MonoBehaviour
+    {
+        public GameData game;
+        public Slider progressBar;
+
+        public void Awake()
+        {
+            game = Game.GetData();
+        }
+        
+        public void Start()
+        {
+            LoadScene(game.CurrentScene);
+        }
+
+        private void LoadScene(string sceneName)
+        {
+            StartCoroutine(DoSceneLoadAsynchronously(sceneName));
+        }
+
+        private IEnumerator DoSceneLoadAsynchronously(string sceneName)
+        {
+            progressBar.value = 0;
+
+            var sceneLoading = SceneManager.LoadSceneAsync(sceneName);
+            while (!sceneLoading.isDone)
+            {
+                var progress = Mathf.Clamp01(sceneLoading.progress / .9f);
+                progressBar.value = progress;
+                yield return null;
+            }
+            
+            game.SetScene(sceneName);
+        }
+    }
+}
